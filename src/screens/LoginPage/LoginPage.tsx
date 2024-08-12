1647a9f47fd467e4bb0481dc5fcdc333c9570eca
selalmaz <ahmetsllmz@gmail.com>
2024-08-12 11:21:34 +0300

    mail ve şifre girerken kontrol eklendi

--- src/screens/LoginPage/LoginPage.tsx
+++ src/screens/LoginPage/LoginPage.tsx
@@ -1,26 +1,43 @@
 import React, {useState} from 'react';
 import {
+  Alert,
   Dimensions,
   Image,
   ImageBackground,
   SafeAreaView,
-  Text,
   View,
 } from 'react-native';
 import Input from '../../components/Input/Input';
 import MyButton from '../../components/Mybutton/Mybutton';
 import style from './LoginPage.style';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {StackParamList} from '../../types';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const {width, height} = Dimensions.get('window');
+  const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
 
   const handleEmailChange = (text: string) => {
     setEmail(text);
   };
   const handlePasswordChange = (password: string) => {
     setPassword(password);
+  };
+
+  const handleLogin = () => {
+    if (!email.trim()) {
+      Alert.alert('Hata', 'Mail adresini giriniz.');
+      return;
+    }
+    if (!password.trim()) {
+      Alert.alert('Hata', 'Şifre giriniz.');
+      return;
+    }
+
+    navigation.navigate('Home');
   };
 
   return (
@@ -41,7 +58,7 @@ const LoginPage = () => {
             secureTextEntry={true}
           />
         </View>
-        <MyButton title="Giriş Yap" navigateTo="Home" />
+        <MyButton title="Giriş Yap" navigateTo="Home" onPress={handleLogin} />
       </SafeAreaView>
     </ImageBackground>
   );
